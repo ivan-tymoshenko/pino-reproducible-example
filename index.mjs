3b67d9029560acbf5d3457ff8531e82a7e26243e
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-09-29 18:05:21 +0200

    test

--- index.mjs
+++ index.mjs
@@ -3,16 +3,22 @@ import { execa } from 'execa'
 
 console.log('Index process')
 
-const destination = pino.destination({
-  dest: 1,
-  sync: false
-})
+const loggers = []
+for (let i = 0; i < 1000; i++) {
+  const destination = pino.destination({
+    dest: 1,
+    sync: false
+  })
+  
+  const logger = pino(destination)
+  loggers.push(logger)
+}
 
-const logger = pino(destination)
+for (let i = 0; i < 10; i++) {
+  const child = execa('echo', [`Child process ${i}`])
+  
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
 
-const child = execa('echo', ['Child process'])
-
-child.stdout.pipe(process.stdout)
-child.stderr.pipe(process.stderr)
-
-await child
+  await child
+}
